b7f9202b86a0a637c5bf3379211f9b7d04a93202
Johann Feser <johannfeser@gmail.com>
2023-02-20 23:08:46 +0100

    Update ScheduleBody.tsx

--- apps/jj_twitch_extension/src/ui/schedule/ScheduleBody.tsx
+++ apps/jj_twitch_extension/src/ui/schedule/ScheduleBody.tsx
@@ -1,7 +1,8 @@
-import {Component, For} from 'solid-js'
-import {useCurrentDay} from './JJScheduleProvider'
+import {Component, For, Match, Switch} from 'solid-js'
+import {useCurrentDay, useSlots} from './JJScheduleProvider'
 import {SlotCard} from './SlotCard'
 import {ScheduleHeader} from './ScheduleHeader'
+import {useCreatorFilter} from "./CreatorFilterProvider";
 
 export const ScheduleBody: Component = () => {
   return (
@@ -14,14 +15,33 @@ export const ScheduleBody: Component = () => {
 
 const ScheduleSlots: Component = () => {
   const slots = () => useCurrentDay().slots
+  const {isEmpty, includes} = useCreatorFilter()
+
+
+  const filteredSlots = () => {
+    return useSlots().filter(s => s.relations.creators.some(includes))
+  }
 
   return (
-    <div class={'pb-10'}>
-      <For each={slots()}>
-        {slot => {
-          return <SlotCard slot={slot} showCountdown={true} showTime={true}/>
-        }}
-      </For>
-    </div>
+    <Switch>
+      <Match when={isEmpty()}>
+        <div class={'pb-10'}>
+          <For each={slots()}>
+            {slot => {
+              return <SlotCard slot={slot} showCountdown={true} showTime={true}/>
+            }}
+          </For>
+        </div>
+      </Match>
+      <Match when={!isEmpty()}>
+        <div class={'pb-10'}>
+          <For each={filteredSlots()}>
+            {slot => {
+              return <SlotCard slot={slot} showCountdown={true} showTime={true}/>
+            }}
+          </For>
+        </div>
+      </Match>
+    </Switch>
   )
 }
