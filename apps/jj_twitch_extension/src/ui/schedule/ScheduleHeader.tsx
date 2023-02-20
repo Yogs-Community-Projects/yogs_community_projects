57639589f7886404061a38b25ecd6232dc50c84d
Johann Feser <johannfeser@gmail.com>
2023-02-20 23:08:37 +0100

    Update ScheduleHeader.tsx

--- apps/jj_twitch_extension/src/ui/schedule/ScheduleHeader.tsx
+++ apps/jj_twitch_extension/src/ui/schedule/ScheduleHeader.tsx
@@ -1,17 +1,22 @@
-import { Component } from 'solid-js'
-import { useCurrentDay, useScheduleData } from './JJScheduleProvider'
-import { DateTime } from 'luxon'
-import { useDayIndexSetter } from './DayIndexProvider'
-import { FaSolidCalendarDay, FaSolidChevronLeft, FaSolidChevronRight } from 'solid-icons/fa'
+import {Component, createEffect, createSignal, Match, Switch} from 'solid-js'
+import {useCurrentDay, useScheduleData} from './JJScheduleProvider'
+import {DateTime} from 'luxon'
+import {useDayIndexSetter} from './DayIndexProvider'
+import {FaSolidCalendarDay, FaSolidChevronLeft, FaSolidChevronRight, FaSolidFilter} from 'solid-icons/fa'
+import {FilterDialog} from "./ScheduleCreatorFilterButton";
+import {useCreatorFilter} from "./CreatorFilterProvider";
+import {createModalSignal, RemoteData, useCreatorDB} from '@ycapp/common'
+import {BiRegularReset} from 'solid-icons/bi'
+import {CreatorData} from '@ycapp/model'
 
 export const ScheduleHeader: Component = () => {
   return (
     <div style={{}} class={'flex w-full flex-col'}>
       <div class={'h-30'}>
-        <Title />
+        <Title/>
       </div>
       <div class={'p-schedule h-10'}>
-        <WeekButtons />
+        <WeekButtons/>
       </div>
     </div>
   )
@@ -20,49 +25,116 @@ export const ScheduleHeader: Component = () => {
 const Title: Component = () => {
   const day = () => useCurrentDay()
   const date = () => DateTime.fromISO(day().start)
+  const {isEmpty, filter} = useCreatorFilter()
+
+  const [creator, setCreator] = createSignal<RemoteData<CreatorData | null>>({
+    data: null,
+    loading: true,
+    error: null
+  })
+
+  createEffect(() => {
+    if (filter().length > 0) {
+      setCreator(useCreatorDB().read(filter()[0]))
+    }
+  })
 
   return (
     <div class={'p-schedule h-full flex-1'}>
       <div class={'schedule-card-white flex h-full flex-col items-center justify-center'}>
         <h3 class={'text-center text-2xl'}>{useScheduleData().name}</h3>
-        <h3 class={'text-center text-2xl'}>
-          {date().toLocaleString({
-            weekday: 'short',
-            day: '2-digit',
-            month: 'short'
-          })}
-        </h3>
+        <Switch>
+          <Match when={isEmpty()}>
+            <h3 class={'text-center text-2xl'}>
+              {date().toLocaleString({
+                weekday: 'short',
+                day: '2-digit',
+                month: 'short'
+              })}
+            </h3>
+          </Match>
+          <Match when={!isEmpty()}>
+            <Switch>
+              <Match when={filter().length == 1 && creator().data}>
+                <h3 class={'text-center text-2xl'}>
+                  Streams with {creator().data.creator.name}
+                </h3>
+              </Match>
+              <Match when={filter().length > 1 && creator().data}>
+                <h2 class={'text-center text-xl'}>
+                  Streams with {creator().data.creator.name} & {filter().length - 1} more streamer
+                </h2>
+              </Match>
+            </Switch>
+          </Match>
+        </Switch>
       </div>
     </div>
   )
 }
 const WeekButtons: Component = () => {
   const [prev, next, today] = useDayIndexSetter()
+  const modalSignal = createModalSignal()
+  const {reset, isEmpty} = useCreatorFilter()
 
   return (
-    <div style={{}} class={`schedule-card-white flex h-full flex-row`}>
-      <button
-        class={
-          'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center rounded-l-2xl hover:scale-105'
-        }
-        onclick={prev}
-      >
-        <FaSolidChevronLeft />
-      </button>
-      <button
-        class={'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center hover:scale-105'}
-        onclick={today}
-      >
-        <FaSolidCalendarDay />
-      </button>
-      <button
-        class={
-          'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center rounded-r-2xl hover:scale-105'
-        }
-        onclick={next}
-      >
-        <FaSolidChevronRight />
-      </button>
-    </div>
+    <>
+      <Switch>
+        <Match when={isEmpty()}>
+          <div style={{}} class={`schedule-card-white flex h-full flex-row`}>
+            <button
+              class={
+                'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center rounded-l-2xl hover:scale-105'
+              }
+              onclick={prev}
+            >
+              <FaSolidChevronLeft/>
+            </button>
+            <button
+              class={'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center hover:scale-105'}
+              onClick={modalSignal.open}
+            >
+              <FaSolidFilter/>
+            </button>
+            <button
+              class={'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center hover:scale-105'}
+              onclick={today}
+            >
+              <FaSolidCalendarDay/>
+            </button>
+            <button
+              class={
+                'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center rounded-r-2xl hover:scale-105'
+              }
+              onclick={next}
+            >
+              <FaSolidChevronRight/>
+            </button>
+          </div>
+        </Match>
+        <Match when={!isEmpty()}>
+          <div style={{}} class={`schedule-card-white flex h-full flex-row`}>
+            <button
+              class={
+                'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center rounded-l-2xl hover:scale-105'
+              } onClick={modalSignal.open}
+            >
+              <FaSolidFilter/>
+            </button>
+            <button
+              class={
+                'hover:bg-accent-50 ripple flex flex-1 flex-col items-center justify-center rounded-r-2xl hover:scale-105'
+              }
+              onclick={() => {
+                reset()
+              }}
+            >
+              <BiRegularReset/>
+            </button>
+          </div>
+        </Match>
+      </Switch>
+      <FilterDialog modalSignal={modalSignal}/>
+    </>
   )
 }
