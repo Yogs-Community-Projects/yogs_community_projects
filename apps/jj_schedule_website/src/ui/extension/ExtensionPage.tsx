cf7e9c7308cc7be0a45911899d3b8167115b1d19
Johann Feser <johannfeser@gmail.com>
2023-02-22 12:16:08 +0100

    update extension pages

--- apps/jj_schedule_website/src/ui/extension/ExtensionPage.tsx
+++ apps/jj_schedule_website/src/ui/extension/ExtensionPage.tsx
@@ -7,14 +7,13 @@ const ExtensionPage: Component = () => {
   return (
     <div class={'mx-auto flex flex-col items-center p-1 text-center text-base text-white md:w-[75%] md:text-2xl'}>
       <p class={'p-2 text-2xl font-bold md:text-4xl'}>Unofficial Jingle Jam Schedule Twitch Extension</p>
-      <p>A new version is in Development</p>
-      <p class={''}>If you want to help and test the extension use these links</p>
+      <p>A new version is in development</p>
+      <p class={'text-xl'}>If you want to help and test the extension use these links.</p>
+      <p class={'text-xl'}>Each tester needs to be given access manually.</p>
       <FeedbackButtons />
-
       <img class={'md:w-[50%]'} src={extensionImg} alt={'Images of the extension'} />
-
       <a class={'p-1 md:p-4'} href={'https://dashboard.twitch.tv/extensions/7m5fitr4o8raxn6fv59lndv2iy0uuz-0.0.4'}>
-        The extension on Twitch
+        The current version of the extension on Twitch
       </a>
       <p>This extension allows any Streamer too add the Yogs Jingle Jam schedule to their channel.</p>
       <p>In addition it show the current amount raised and a list of Streamers participating.</p>
@@ -33,6 +32,19 @@ const ExtensionPage: Component = () => {
         The Extension was added to the <a href={'https://www.twitch.tv/yogscast'}>Yogscast channel</a> on Dec 1. 2022
         during the OTTD Stream. It was used by 70K+ people during the two weeks of Jingle Jam.
       </p>
+      <div class={'justify-start text-left'}>
+        <p>Twitch extension limitations</p>
+        <ul class={'list-disc text-base'}>
+          <li>Every update must be reviewed by Twitch which can take up to 3 days</li>
+          <li>No outgoing links are allow except Twitch links</li>
+          <ul class={'ml-2 list-disc'}>
+            <li>YouTube</li>
+            <li>Social Media</li>
+            <li>Merch</li>
+            <li>Sites where you can spend money (incl. charity afaik)</li>
+          </ul>
+        </ul>
+      </div>
     </div>
   )
 }
--- apps/yogs_schedule_website/src/ui/extension/ExtensionPage.tsx
+++ apps/yogs_schedule_website/src/ui/extension/ExtensionPage.tsx
@@ -9,7 +9,8 @@ const ExtensionPage: Component = () => {
     <div class={'mx-auto flex flex-col items-center p-1 text-center text-base text-white md:w-[75%] md:text-2xl'}>
       <p class={'p-2 text-2xl font-bold md:text-4xl'}>Unofficial Yogscast Stream Team Twitch Extension</p>
       <p class={'font-bold'}>This extension is still in development</p>
-      <p class={''}>If you want to help and test the extension use these links</p>
+      <p class={'text-xl'}>If you want to help and test the extension use these links.</p>
+      <p class={'text-xl'}>Each tester needs to be given access manually.</p>
       <FeedbackButtons />
       <div class={'flex flex-wrap items-start justify-center gap-2'}>
         <div class={'flex w-52 flex-col items-center justify-center'}>
@@ -25,11 +26,22 @@ const ExtensionPage: Component = () => {
           <p class={'text-base'}>Who is part of a stream</p>
         </div>
       </div>
-      <div class={'text-left'}>
-        <p>Planed Features</p>
+      <div class={'justify-start text-left'}>
+        <p>Currently planed features</p>
         <ul class={'list-disc text-base'}>
           <li>Way to show special one-of streams</li>
           <li>Way to add/switch to a special stream/events schedule</li>
+        </ul>
+        <p>Twitch extension limitations</p>
+        <ul class={'list-disc text-base'}>
+          <li>Every update must be reviewed by Twitch which can take up to 3 days</li>
+          <li>No outgoing links are allow except Twitch links</li>
+          <ul class={'ml-2 list-disc'}>
+            <li>YouTube</li>
+            <li>Social Media</li>
+            <li>Merch</li>
+            <li>Sites where you can spend money (incl. charity afaik)</li>
+          </ul>
         </ul>
       </div>
     </div>
