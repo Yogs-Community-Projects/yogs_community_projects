e479e0a20876a2a029bc1e3ba5c6da0441261eef
Johann Feser <johannfeser@gmail.com>
2023-02-24 21:57:28 +0100

    Update vite.config.ts

--- apps/jj_schedule_website/vite.config.ts
+++ apps/jj_schedule_website/vite.config.ts
@@ -3,15 +3,14 @@ import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [
-    solidPlugin()
+    solidPlugin(),
     // splitVendorChunkPlugin(),
-    // @ts-ignore
     // VitePWA(pwaOptions()),
   ],
   server: {
-    port: 3000
+    port: 3000,
   },
   build: {
-    target: 'esnext'
-  }
+    target: 'esnext',
+  },
 })
