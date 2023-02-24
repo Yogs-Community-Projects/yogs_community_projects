5abf8a31e962527f9597d38ed96e1705289b5785
Johann Feser <johannfeser@gmail.com>
2023-02-24 21:59:29 +0100

    Update SchedulePage.tsx

--- apps/yogs_twitch_extension/src/ui/schedule/SchedulePage.tsx
+++ apps/yogs_twitch_extension/src/ui/schedule/SchedulePage.tsx
@@ -1,25 +1,29 @@
-import { Component, Match, Switch } from 'solid-js'
+import { Component, createMemo, Match, Switch } from 'solid-js'
 import { ScheduleDataProvider } from './YogsScheduleProvider'
 import { DayIndexProvider } from './DayIndexProvider'
 import { ScheduleBody } from './ScheduleBody'
 import { useScheduleDB } from '@ycapp/common'
+import { useYogsExtensionConfig } from '../components/YogsExtensionConfigProvider'
 
 const SchedulePage: Component = () => {
-  const schedule = useScheduleDB().readYogs()
+  const id = () => useYogsExtensionConfig().scheduleId
+  const schedule = createMemo(() => useScheduleDB().read(id()))
+  // yogscaststreamteam
+  // tinyteams2022
 
   return (
     <Switch>
-      <Match when={schedule.data} keyed={true}>
-        <ScheduleDataProvider scheduleData={schedule.data}>
+      <Match when={schedule().data} keyed={true}>
+        <ScheduleDataProvider scheduleData={schedule().data}>
           <DayIndexProvider>
             <ScheduleBody />
           </DayIndexProvider>
         </ScheduleDataProvider>
       </Match>
-      <Match when={schedule.error} keyed={false}>
-        <p>{JSON.parse(schedule.error)}</p>
+      <Match when={schedule().error} keyed={false}>
+        <p>{JSON.parse(schedule().error)}</p>
       </Match>
-      <Match when={schedule.loading} keyed={false}>
+      <Match when={schedule().loading} keyed={false}>
         <p>Loading...</p>
       </Match>
     </Switch>
