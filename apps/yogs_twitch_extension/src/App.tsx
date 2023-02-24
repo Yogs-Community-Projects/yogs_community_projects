e114d720675f1078fbb5fb7b8c571438708c3a7d
Johann Feser <johannfeser@gmail.com>
2023-02-24 21:59:10 +0100

    Update App.tsx

--- apps/yogs_twitch_extension/src/App.tsx
+++ apps/yogs_twitch_extension/src/App.tsx
@@ -1,14 +1,29 @@
-import { Component } from 'solid-js'
+import { useConfigDB } from '@ycapp/common'
+import { Component, Match, Switch } from 'solid-js'
 import AppBody from './AppBody'
 import NavBar from './ui/components/NavBar'
+import { YogsExtensionConfigProviderProvider } from './ui/components/YogsExtensionConfigProvider'
 
 const App: Component = () => {
+  const config = useConfigDB().readYogsExtensionConfig()
   return (
     <div class={'flex min-h-screen flex-col items-center'}>
-      <NavBar />
-      <div class={'container mx-auto'}>
-        <AppBody />
-      </div>
+      <Switch>
+        <Match when={config.data}>
+          <YogsExtensionConfigProviderProvider config={config.data}>
+            <NavBar />
+            <div class={'container mx-auto'}>
+              <AppBody />
+            </div>
+          </YogsExtensionConfigProviderProvider>
+        </Match>
+        <Match when={config.loading}>
+          <p>Loading Config...</p>
+        </Match>
+        <Match when={config.error}>
+          <p>Error: {JSON.stringify(config.error)}</p>
+        </Match>
+      </Switch>
     </div>
   )
 }
