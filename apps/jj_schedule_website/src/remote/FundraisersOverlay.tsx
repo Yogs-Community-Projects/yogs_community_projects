a0e64270b123373cd5c17238d51c71b2dd56a320
Johann Feser <johannfeser@gmail.com>
2023-07-07 23:05:39 +0200

    Use marquee animation

--- apps/jj_schedule_website/src/remote/FundraisersOverlay.tsx
+++ apps/jj_schedule_website/src/remote/FundraisersOverlay.tsx
@@ -1,25 +1,71 @@
-import { Component, createSignal, onCleanup, Suspense, useTransition } from 'solid-js'
+import { Component, For, JSXElement } from 'solid-js'
 import { data } from '../assets/fundraiser_data.json'
 import { Numeric } from 'solid-i18n'
 import { FaBrandsTwitch } from 'solid-icons/fa'
 import { excludedChannel } from './overlay_signals'
 
 export const FundraisersOverlay: Component = () => {
-  const [pending, start] = useTransition()
   const e = excludedChannel()
-
   const useData = () => data.filter(d => !e.includes(d.login))
-  const length = () => useData().length
-  const [index, setIndex] = createSignal(0)
-  const current = () => useData()[index()]
-  const timer = setInterval(() => start(() => setIndex((index() + 1) % length())), 8000)
-  onCleanup(() => clearInterval(timer))
+
+  const desc = () => {
+    if (useData().length % 4 == 0) {
+      return 4
+    }
+    return 3
+  }
+  const items = () => {
+    const lst = useData()
+    const result: JSXElement[] = []
+
+    for (let i = 0; i < lst.length; i++) {
+      const d = lst[i]
+      if (i % desc() == 0) {
+        result.push(
+          <div class={'flex h-full w-full flex-col items-center justify-center rounded-2xl bg-white p-2 shadow-2xl'}>
+            <p class={'text-accent-500 font-bold'}>Community</p>
+            <p class={'text-primary-500 font-bold'}>Fundraisers</p>
+          </div>,
+        )
+      }
+      result.push(
+        <div class={'h-full w-full rounded-2xl bg-white p-2 shadow-2xl'}>
+          <div class={'flex h-full w-full flex-row items-center justify-start'}>
+            <img class={'h-12 w-12 rounded-lg'} alt={''} src={d.img} />
+            <div class={'flex h-full w-full flex-col items-start justify-center pl-2'}>
+              <div class={'text-accent flex flex-row items-center font-bold'}>
+                <FaBrandsTwitch size={18} /> {' /'}
+                {d.login}
+              </div>
+              <p class={'text-primary font-bold'}>
+                Raised <Numeric value={+d.amount} numberStyle="currency" currency={'GBP'} />
+              </p>
+            </div>
+          </div>
+        </div>,
+      )
+    }
+
+    return result
+  }
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div class={'h-16 w-full p-2'} classList={{ pending: pending() }}>
-        <Child data={current()} />
+    <div class="relative flex overflow-x-hidden">
+      <div class="animate-marquee flex flex-row whitespace-nowrap">
+        <For each={items()}>
+          {d => {
+            return <div class="inline-block h-[72px] w-[256px] items-center justify-center px-2 py-1">{d}</div>
+          }}
+        </For>
       </div>
-    </Suspense>
+      <div class="animate-marquee2 absolute top-0 flex flex-row whitespace-nowrap">
+        <For each={items()}>
+          {d => {
+            return <div class="inline-block h-[72px] w-[256px] items-center justify-center px-2 py-1">{d}</div>
+          }}
+        </For>
+      </div>
+    </div>
   )
 }
 
@@ -65,7 +111,7 @@ interface ChildProps {
 
 const Child: Component<ChildProps> = props => {
   return (
-    <div class={'text-xxl rounded-2xl bg-white shadow-xl'}>
+    <div class={'h-full w-full rounded-2xl bg-white shadow-xl'}>
       <div class={'flex w-full items-center p-1'}>
         <img class={'h-12 w-12 rounded-lg'} alt={''} src={props.data.img} />
         <div class={'w-full pl-1'}>
