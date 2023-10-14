50a0e768069fcb21f64385a94bf33b2a5ba5c711
Johann Feser <johannfeser@gmail.com>
2023-10-14 16:12:18 +0200

    Update InvisibleBody.tsx

--- apps/jj_twitch_extension/src/ui/InvisibleBody.tsx
+++ apps/jj_twitch_extension/src/ui/InvisibleBody.tsx
@@ -1,8 +1,7 @@
-import { Component, createSignal, onCleanup } from 'solid-js'
-import { Accessor } from 'solid-js/types/reactive/signal'
-import { DateTime, Duration } from 'luxon'
+import { Component, Show } from 'solid-js'
 import { useTheme } from '../ThemeProvider'
 import { twMerge } from 'tailwind-merge'
+import { useIsJJ, useJJStartCountdown, useNextJJStartDate } from '@ycapp/common'
 
 interface InvisibleBodyProps {
   text: string
@@ -10,7 +9,9 @@ interface InvisibleBodyProps {
 
 export const InvisibleBody: Component<InvisibleBodyProps> = props => {
   const { theme } = useTheme()
-
+  const nextJJStartDate = useNextJJStartDate()
+  const jjCountdown = useJJStartCountdown()
+  const isJJ = useIsJJ()
   return (
     <div class={'p-1'}>
       <div
@@ -19,77 +20,19 @@ export const InvisibleBody: Component<InvisibleBodyProps> = props => {
           theme() === 'rainbow' ? 'rounded-2xl bg-gray-500/50' : '',
         )}
       >
-        <p class={'p-1 text-2xl font-bold md:p-2 md:text-4xl'}>Jingle Jam Countdown</p>
-        <p class={'text-xl md:text-3xl'}>{useNextJJStartDate().toLocal().toFormat('DDDD')}</p>
-        <p class={'text-xl md:text-3xl'}>{useNextJJStartDate().toLocal().toFormat('ttt')}</p>
-        <p class={''}>{useNextJJStartDate().toFormat('DDDD')}</p>
-        <p class={''}>{useNextJJStartDate().toFormat('ttt')}</p>
-        <div class={'flex flex-col items-center p-1 text-white md:p-4'}>
-          <p class={'text-2xl md:text-4xl'}>Jingle Jam {useNextJJStartDate().year} starts</p>
-          <p class={'font-mono text-2xl md:text-4xl'}>{useJJStartCountdown().toFormat("dd 'Days' hh:mm:ss")}</p>
-        </div>
+        <Show when={!isJJ()}>
+          <p class={'p-1 text-2xl font-bold md:p-2 md:text-4xl'}>Jingle Jam Countdown</p>
+          <p class={'text-xl md:text-3xl'}>{nextJJStartDate().toLocal().toFormat('DDDD')}</p>
+          <p class={'text-xl md:text-3xl'}>{nextJJStartDate().toLocal().toFormat('ttt')}</p>
+          <p class={''}>{nextJJStartDate().toFormat('DDDD')}</p>
+          <p class={''}>{nextJJStartDate().toFormat('ttt')}</p>
+          <div class={'flex flex-col items-center p-1 text-white md:p-4'}>
+            <p class={'text-2xl md:text-4xl'}>Jingle Jam {nextJJStartDate().year} starts</p>
+            <p class={'font-mono text-2xl md:text-4xl'}>{jjCountdown().toFormat("dd 'Days' hh:mm:ss")}</p>
+          </div>
+        </Show>
         <p>{props.text}</p>
       </div>
     </div>
   )
-}
-
-const london = 'Europe/London'
-export const useDatetimeLondonNow: Accessor<DateTime> = (init?: DateTime) => {
-  const [date, setDate] = createSignal(init ?? DateTime.now().setZone(london))
-  const interval = setInterval(() => setDate(init ?? DateTime.now().setZone(london)), 1000)
-  onCleanup(() => clearInterval(interval))
-  return date()
-}
-export const useDatetimeNow: Accessor<DateTime> = () => {
-  const [date, setDate] = createSignal(DateTime.now())
-  const interval = setInterval(() => setDate(DateTime.now()), 1000)
-  onCleanup(() => clearInterval(interval))
-  return date()
-}
-export const useNextJJStartDate: Accessor<DateTime> = () => {
-  if (!useIsAfterJJEnd()) {
-    return DateTime.fromObject(
-      {
-        year: useDatetimeLondonNow().year,
-        month: 12,
-        day: 1,
-        hour: 17,
-      },
-      {
-        zone: london,
-      },
-    )
-  }
-  return DateTime.fromObject(
-    {
-      year: useDatetimeLondonNow().year + 1,
-      month: 12,
-      day: 1,
-      hour: 17,
-    },
-    {
-      zone: london,
-    },
-  )
-}
-export const useIsAfterJJEnd: Accessor<boolean> = () => {
-  const end = DateTime.fromObject(
-    {
-      year: useDatetimeLondonNow().year,
-      month: 12,
-      day: 14,
-      hour: 23,
-      minute: 59,
-      second: 59,
-    },
-    {
-      zone: london,
-    },
-  )
-  return end < useDatetimeLondonNow()
-}
-
-export const useJJStartCountdown: Accessor<Duration> = () => {
-  return useNextJJStartDate().diff(useDatetimeNow())
 }
