1c14f328e6f5e9062d1339f8ed2adde2cae1cbf3
Johann Feser <johannfeser@gmail.com>
2023-10-15 16:58:01 +0200

    Update Providers.tsx

--- apps/jj_twitch_extension/src/Providers.tsx
+++ apps/jj_twitch_extension/src/Providers.tsx
@@ -1,12 +1,19 @@
-import { lazy, ParentComponent } from 'solid-js'
+import { ParentComponent } from 'solid-js'
 import { createI18n, I18nProvider } from 'solid-i18n'
 import { useLocale } from '@kobalte/core'
+import DBWrapper from './db_wrapper'
+import JJConfigProviderLoader from './JJConfigProviderLoader'
+import TwitchConfigProvider from './ui/config/TwitchConfigProvider'
+import ThemeProvider from './ui/themeProvider/src/ThemeProvider'
+import DataProvider from './ui/dataProvider/src/DataProvider'
 
+/*
 const DBWrapper = lazy(() => import('./db_wrapper'))
 const JJConfigProviderLoader = lazy(() => import('./JJConfigProviderLoader'))
 const TwitchConfigProvider = lazy(() => import('./ui/config/TwitchConfigProvider'))
 const DataProvider = lazy(() => import('./ui/dataProvider/src/DataProvider'))
 const ThemeProvider = lazy(() => import('./ui/themeProvider/src/ThemeProvider'))
+*/
 const Providers: ParentComponent = props => {
   const i18n = createI18n({ language: useLocale().locale() })
   return (
