3f5b5e8d9e95e09ef1cc3a57eacde5e0fbf66384
Johann Feser <johannfeser@gmail.com>
2023-10-31 22:48:30 +0100

    Update ScheduleHeader.tsx

--- apps/jj_twitch_extension/src/ui/schedule/ScheduleHeader.tsx
+++ apps/jj_twitch_extension/src/ui/schedule/ScheduleHeader.tsx
@@ -23,7 +23,7 @@ export const Title: Component = () => {
   })
 
   return (
-    <div class={'h-full flex-1 p-2'}>
+    <div class={'h-full flex-1 px-2'}>
       <div class={'schedule-card-white flex h-full flex-col items-center justify-center'}>
         <h3 class={'text-center text-xl'}>{useScheduleData().name}</h3>
         <Switch>
