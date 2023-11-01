ea69628c3f88732262fc55fcccb255d00307cfc9
Johann Feser <johannfeser@gmail.com>
2023-11-01 16:01:01 +0100

    Update InvisibleBody.tsx

--- apps/jj_twitch_extension/src/ui/InvisibleBody.tsx
+++ apps/jj_twitch_extension/src/ui/InvisibleBody.tsx
@@ -27,8 +27,8 @@ export const InvisibleBody: Component<InvisibleBodyProps> = props => {
           <p class={''}>{nextJJStartDate().toFormat('DDDD')}</p>
           <p class={''}>{nextJJStartDate().toFormat('ttt')}</p>
           <div class={'flex flex-col items-center p-1 text-white md:p-4'}>
-            <p class={'text-2xl md:text-4xl'}>Jingle Jam {nextJJStartDate().year} starts</p>
-            <p class={'font-mono text-2xl md:text-4xl'}>{jjCountdown().toFormat("dd 'Days' hh:mm:ss")}</p>
+            <p class={'text-2xl md:text-4xl'}>Jingle Jam {nextJJStartDate().year} starts in</p>
+            <p class={'font-mono text-2xl md:text-4xl'}>{jjCountdown().toFormat("dd'd' hh'h' mm'm' ss's'")}</p>
           </div>
         </Show>
         <p>{props.text}</p>
