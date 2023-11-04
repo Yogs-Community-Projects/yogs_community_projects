fdebd231044715304f0aeabac11bbbd13e5fe538
Johann Feser <johannfeser@gmail.com>
2023-11-04 23:06:01 +0100

    Update index.tsx

--- apps/jj_schedule_website/src/index.tsx
+++ apps/jj_schedule_website/src/index.tsx
@@ -4,7 +4,8 @@ import './index.css'
 import { Component, lazy } from 'solid-js'
 import { useLocale } from '@kobalte/core'
 import { createI18n, I18nProvider } from 'solid-i18n'
-
+import { ConfigProviderLoader } from './ui/configProvider/ConfigProvider'
+import { MetaProvider } from '@solidjs/meta'
 // const App = lazy(() => import('./App'))
 const DBWrapper = lazy(() => import('./db_wrapper'))
 const App = lazy(() => import('./App'))
@@ -12,11 +13,15 @@ const App = lazy(() => import('./App'))
 const Main: Component = () => {
   const i18n = createI18n({ language: useLocale().locale() })
   return (
-    <I18nProvider i18n={i18n}>
-      <DBWrapper>
-        <App />
-      </DBWrapper>
-    </I18nProvider>
+    <MetaProvider>
+      <I18nProvider i18n={i18n}>
+        <DBWrapper>
+          <ConfigProviderLoader>
+            <App />
+          </ConfigProviderLoader>
+        </DBWrapper>
+      </I18nProvider>
+    </MetaProvider>
   )
 }
 
