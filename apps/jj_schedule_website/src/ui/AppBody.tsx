b1dc7a3d1c59e8e5c3ab9c144aeba5f33c427d4c
Johann Feser <johannfeser@gmail.com>
2023-11-05 22:13:21 +0100

    Update AppBody.tsx

--- apps/jj_schedule_website/src/ui/AppBody.tsx
+++ apps/jj_schedule_website/src/ui/AppBody.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'solid-js'
 import { Outlet } from '@solidjs/router'
-import { AnalyticsConsent } from './components/AnalyticsConsent'
+import { AnalyticsConsentBanner } from './components/AnalyticsConsent'
 import { JJLogo } from './components/JJLogo'
 import { NavBar } from './components/NavBar'
 import { Footer } from './components/Footer'
@@ -10,9 +10,9 @@ const AppBody: Component = () => {
     <div
       class={'font-poppins from-primary-300 to-primary-700 flex min-h-screen flex-col items-center bg-gradient-to-b'}
     >
-      <AnalyticsConsent />
       <JJLogo />
       <NavBar />
+      <AnalyticsConsentBanner />
       <div class={'container mx-auto sm:p-1'}>
         <Outlet />
       </div>
