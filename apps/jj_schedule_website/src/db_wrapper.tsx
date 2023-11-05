3238fca5f973dc6e43fc8d9fdee17952c461cff8
Johann Feser <johannfeser@gmail.com>
2023-11-05 22:13:15 +0100

    Update db_wrapper.tsx

--- apps/jj_schedule_website/src/db_wrapper.tsx
+++ apps/jj_schedule_website/src/db_wrapper.tsx
@@ -2,7 +2,7 @@ import { YcDBProvider } from '@ycapp/common'
 import { ParentComponent } from 'solid-js'
 
 const DBWrapper: ParentComponent = props => {
-  return <YcDBProvider initAnalytics={true}>{props.children}</YcDBProvider>
+  return <YcDBProvider initAnalytics={false}>{props.children}</YcDBProvider>
 }
 
 export default DBWrapper
