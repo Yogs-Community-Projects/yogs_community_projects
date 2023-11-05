a9f2c130ddac2afabb3d2a90245d6a912b71b041
Johann Feser <johannfeser@gmail.com>
2023-11-05 21:09:51 +0100

    Update analytics_util.ts

--- apps/jj_schedule_website/src/analytics_util.ts
+++ apps/jj_schedule_website/src/analytics_util.ts
@@ -1,18 +1,84 @@
-import { gtag } from 'ga-gtag'
+import { Creator, Slot, SlotUtils } from '@ycapp/model'
+import { gtag, install } from 'ga-gtag'
+import { logEvent, setAnalyticsCollectionEnabled, setConsent } from '@firebase/analytics'
+import { useFBAnalytics } from '@ycapp/common'
 
-export function logSlotClick(year, day, slot, title) {
-  gtag('event', 'click_slot', {
-    slot_title: title,
-    event_label: `${year}_${day}_${slot}`,
-  })
-}
+export const useAnalytics = () => {
+  const analytics = useFBAnalytics()
 
-export function logCreator(name) {
-  try {
-    gtag('event', 'click_creator', {
-      name: name,
+  const installGTag = () => {
+    install(import.meta.env.VITE_GTAG, { anonymize_ip: true })
+    gtag('consent', 'default', {
+      ad_storage: 'denied',
+      analytics_storage: 'denied',
+      functionality_storage: 'denied',
+      personalization_storage: 'denied',
+      security_storage: 'denied',
     })
-  } catch (e) {
-    console.error(e)
+    setConsent({
+      ad_storage: 'denied',
+      analytics_storage: 'granted',
+      functionality_storage: 'granted',
+      personalization_storage: 'denied',
+      security_storage: 'denied',
+    })
   }
+
+  const log = (eventName: string, data: { [key: string]: any }) => {
+    gtag('event', eventName, data)
+    logEvent(analytics, eventName, data)
+  }
+
+  const logSlotClick = (slot: Slot) => {
+    try {
+      const start = SlotUtils.start(slot)
+      const data = {
+        slot_title: slot.title,
+        slot_year: start.year,
+        event_label: `${start.year}_${start.day}_${slot}`,
+      }
+      log('click_slot', data)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  const logCreator = (creator: Creator) => {
+    try {
+      log('click_creator', {
+        name: creator.name,
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  const grantConsent = () => {
+    setAnalyticsCollectionEnabled(analytics, true)
+    gtag('consent', 'update', {
+      analytics_storage: 'granted',
+      functionality_storage: 'granted',
+    })
+    setConsent({
+      ad_storage: 'denied',
+      analytics_storage: 'granted',
+      functionality_storage: 'granted',
+      personalization_storage: 'denied',
+      security_storage: 'denied',
+    })
+  }
+  const deniedConsent = () => {
+    setAnalyticsCollectionEnabled(analytics, false)
+    gtag('consent', 'update', {
+      analytics_storage: 'denied',
+      functionality_storage: 'denied',
+    })
+    setConsent({
+      ad_storage: 'denied',
+      analytics_storage: 'denied',
+      functionality_storage: 'denied',
+      personalization_storage: 'denied',
+      security_storage: 'denied',
+    })
+  }
+
+  return { installGTag, logSlotClick, logCreator, log, grantConsent, deniedConsent }
 }
