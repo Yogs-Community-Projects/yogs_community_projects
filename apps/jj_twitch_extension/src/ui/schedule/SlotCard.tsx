9071a255524114a8a3eb18c2a97ac89d87c73faf
Johann Feser <johannfeser@gmail.com>
2023-11-07 22:08:12 +0100

    Update SlotCard.tsx

--- apps/jj_twitch_extension/src/ui/schedule/SlotCard.tsx
+++ apps/jj_twitch_extension/src/ui/schedule/SlotCard.tsx
@@ -68,6 +68,69 @@ export const SlotCard: Component<SlotCardProps> = props => {
 
   const modalSignal = createModalSignal()
   const hasSubtitle = () => props.slot.subtitle && props.slot.subtitle?.length > 0
+
+  return (
+    <>
+      <div
+        class={
+          'hover:scale-102 relative flex h-[68px] cursor-pointer flex-col items-center justify-between rounded-2xl p-2 shadow-2xl transition-all hover:brightness-105'
+        }
+        style={{
+          ...background(),
+        }}
+        onclick={modalSignal.toggle}
+      >
+        <div class={'absolute top-0 flex h-full w-full flex-col items-center justify-center px-4 py-2'}>
+          <Show when={!hasSubtitle()}>
+            <p class={'line-clamp-2 w-full text-center text-sm font-bold uppercase'}>{props.slot.title}</p>
+          </Show>
+          <Show when={hasSubtitle()}>
+            <p class={'line-clamp-1 w-full text-center text-sm font-bold uppercase'}>{props.slot.title}</p>
+          </Show>
+          <Show when={props.slot.subtitle && props.slot.subtitle.length > 0}>
+            <p class={'line-clamp-1 text-center text-xs uppercase'}>{props.slot.subtitle}</p>
+          </Show>
+          <Show when={props.showTime && isOver()}>
+            <p class={'line-clamp-1 text-center text-xs'}>
+              {nextStream().toLocaleString({
+                hour: 'numeric',
+                minute: 'numeric',
+                timeZoneName: 'short',
+              })}
+            </p>
+          </Show>
+          <Show when={props.showCountdown && isBefore()}>
+            <p class={'line-clamp-1 text-center text-xs'}>
+              <span class={'font-mono'}>{countdownFormat()}</span>,{' '}
+              {nextStream().toLocaleString({
+                hour: 'numeric',
+                minute: 'numeric',
+                timeZoneName: 'short',
+              })}
+            </p>
+          </Show>
+          <Show when={isLive()}>
+            <p class={'text-xs'}>LIVE</p>
+          </Show>
+        </div>
+        <div class={'absolute bottom-0 right-0 p-2'}>
+          <Show when={slot.relations.creators.length > 0}>
+            <BsPeopleFill size={12} />
+          </Show>
+        </div>
+        <div class={'absolute bottom-0 left-0 p-2'}>
+          <Show when={(slot.showTwitchIcon || slot.showHighlightIcon) && isOver()}>
+            <p class={'text-xxs font-bold'}>VOD</p>
+          </Show>
+        </div>
+      </div>
+      <Suspense>
+        <SlotDialog slot={slot} modalSignal={modalSignal} />
+      </Suspense>
+    </>
+  )
+
+  /*
   return (
     <>
       <div
@@ -117,5 +180,5 @@ export const SlotCard: Component<SlotCardProps> = props => {
         <SlotDialog slot={slot} modalSignal={modalSignal} />
       </Suspense>
     </>
-  )
+  )*/
 }
