4f2bbe1e0310beaac4e30e68ecdd48a37e70cfda
Johann Feser <johannfeser@gmail.com>
2023-11-09 19:39:49 +0100

    replace clock icon with Y

--- apps/jj_twitch_extension/src/ui/components/NavBar.tsx
+++ apps/jj_twitch_extension/src/ui/components/NavBar.tsx
@@ -6,6 +6,30 @@ import { useTheme } from '../themeProvider'
 import { useTwitchConfig } from '../config/useTwitchConfig'
 import { TabType } from '../config/TwitchConfig'
 
+const Yogs = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 1163 1268">
+      <path
+        id="Auswahl"
+        fill="#ffffff"
+        stroke="white"
+        stroke-width="1"
+        d="M 712.00,1.00
+           C 719.54,2.41 1042.00,172.00 1042.00,172.00
+             1042.00,172.00 806.56,637.00 806.56,637.00
+             806.56,637.00 950.00,1176.00 950.00,1176.00
+             950.00,1176.00 586.72,1265.88 586.72,1265.88
+             586.72,1265.88 442.57,731.10 442.57,731.10
+             442.57,731.10 0.00,496.00 0.00,496.00
+             0.00,496.00 190.00,144.00 190.00,144.00
+             190.00,144.00 545.00,365.00 545.00,365.00
+             545.00,365.00 712.00,1.00 712.00,1.00 Z
+           M 702.00,576.00"
+      />
+    </svg>
+  )
+}
+
 const NavBar: Component = () => {
   const { config } = useTwitchConfig()
   const tab1 = () => config.tab1
@@ -34,7 +58,7 @@ const TabIcon: Component<{ tab: TabType }> = props => {
   return (
     <Switch>
       <Match when={props.tab === 'yogs'}>
-        <FaRegularClock />
+        <Yogs />
       </Match>
       <Match when={props.tab === 'charities'}>
         <FaSolidHeart />
