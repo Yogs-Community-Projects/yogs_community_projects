829ca7cab17d99b3cefabdccd6ec62d18b0a771a
Johann Feser <johannfeser@gmail.com>
2023-12-04 20:02:50 +0100

    Update WeekIndexProvider.tsx

--- apps/jj_schedule_website/src/ui/schedule/providers/WeekIndexProvider.tsx
+++ apps/jj_schedule_website/src/ui/schedule/providers/WeekIndexProvider.tsx
@@ -1,5 +1,8 @@
 import { createContext, createSignal, ParentComponent, useContext } from 'solid-js'
 import { Accessor } from 'solid-js/types/reactive/signal'
+import { DateTime } from 'luxon'
+import { useWeeks } from './ScheduleDataProvider'
+import { useIsJJ } from '@ycapp/common'
 
 type WeekIndexProps = {
   useIndex: Accessor<number>
@@ -9,7 +12,16 @@ type WeekIndexProps = {
 export const WeekIndexContext = createContext<WeekIndexProps>()
 
 export const WeekIndexProvider: ParentComponent = props => {
-  const [useIndex, setIndex] = createSignal(0)
+  const jj = useIsJJ()
+  const weeks = useWeeks()
+  const now = DateTime.now()
+  let day = Math.floor((now.day - 1) / 7) % weeks.length
+  if (day > weeks.length && !jj()) {
+    day = 0
+  }
+  console.log('day', day)
+  const [useIndex, setIndex] = createSignal(day)
+
   const prev = () => {
     setIndex(Math.abs((useIndex() - 1) % 2))
   }
@@ -19,6 +31,6 @@ export const WeekIndexProvider: ParentComponent = props => {
 
   return <WeekIndexContext.Provider value={{ useIndex, prev, next }}>{props.children}</WeekIndexContext.Provider>
 }
-export const useWeekIndexContext = () => useContext(WeekIndexContext)!
+export const useWeekIndexContext = () => useContext(WeekIndexContext)
 export const useWeekIndex: Accessor<number> = () => useWeekIndexContext().useIndex()
 export const useWeekIndexSetter = () => [useWeekIndexContext().prev, useWeekIndexContext().next]
