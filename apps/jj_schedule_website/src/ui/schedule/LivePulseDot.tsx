89982362b3537d26fb8e9920f8548f5fb78c5ae4
Johann Feser <johannfeser@gmail.com>
2023-12-05 12:12:55 +0100

    Update LivePulseDot.tsx

--- apps/jj_schedule_website/src/ui/schedule/LivePulseDot.tsx
+++ apps/jj_schedule_website/src/ui/schedule/LivePulseDot.tsx
@@ -1,17 +1,13 @@
-import { twMerge } from 'tailwind-merge'
-
 export const LivePulseDot = () => {
   return (
     <div class={'bg-accent-500 flex flex-row items-center justify-center gap-2 rounded px-1'}>
-      <p class={'text-xs font-bold tracking-wide'}>LIVE</p>
+      <p class={'text-xs font-bold tracking-wide text-white'}>LIVE</p>
       <div class={'h-2 w-2'}>
         <span class="relative flex h-2 w-2">
           <span
-            class={twMerge(
-              'absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75 duration-700',
-            )}
+            class={'absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75 duration-700'}
           />
-          <span class={twMerge('relative inline-flex h-full w-full rounded-full bg-red-500')} />
+          <span class={'relative inline-flex h-full w-full rounded-full bg-red-500'} />
         </span>
       </div>
     </div>
